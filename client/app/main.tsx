83b38913c2e004f4bd2b5ae87393a2e5ba90cae5
Nipuna NIshan <nipunanishan47@gmail.com>
2024-11-30 01:15:38 +0530

    Refactor routing structure and update type definitions for improved clarity and organization

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './globals.css'
-import { BrowserRouter, Routes } from 'react-router'
-import { flattenRoutes } from '@/lib/routes-helper'
-import { routesConfig } from '@/routes'
-import Layout from './layout'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './globals.css';
+import { BrowserRouter, Routes } from 'react-router';
+import { flattenRoutes } from '@/lib/router';
+import { routesConfig } from '@/routes';
+import Layout from './layout';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,4 +14,4 @@ createRoot(document.getElementById('root')!).render(
       </BrowserRouter>
     </Layout>
   </StrictMode>
-)
+);
---
+++ client/types/api/index.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Error } from './models/Error';
+export type { Movie } from './models/Movie';
+export { Status } from './models/Status';
+
+export { DownloadsService } from './services/DownloadsService';
+export { MoviesService } from './services/MoviesService';
+export { StatusService } from './services/StatusService';
+export { StreamingService } from './services/StreamingService';
